e148d32a8f7460519884f01f7faa43fa615df0a3
Dario Piriz <Dario.Piriz@qualitestgroup.com>
2022-06-01 20:15:02 -0300

    Subi un modelo parcial del final

---
+++ Final/SP-Bikes/SP-Bikes/Models/Rol.cs
@@ -0,0 +1,11 @@
+﻿namespace SP_Bikes.Models
+{
+    public class Rol
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public bool Activo { get; set; }
+        
+    }
+}
